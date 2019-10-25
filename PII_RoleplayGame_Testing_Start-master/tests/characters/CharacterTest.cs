ff75ffd9fd6284acf27f4e23848394d9575365e9
SebaColina <sebacoli00@gmail.com>
2019-10-25 20:10:08 -0300

    fix Conflic

--- PII_RoleplayGame_Testing_Start-master/tests/characters/CharacterTest.cs
+++ PII_RoleplayGame_Testing_Start-master/tests/characters/CharacterTest.cs
@@ -1,71 +1,57 @@
 using NUnit.Framework;
-using RoleplayGame.Items;
 using RoleplayGame.Characters;
+using RoleplayGame.Items;
 
 namespace RoleplayGame.Library.Test
 {
-    public class CharacterTest
-    {
-        [Test]
-        public void TestDeath()
-        {
-            //Desafio 6
-            Wizard wizard = new Wizard("wizard");
+         public class CharacterTest
+         {
+                  [Test]
+                  public void RemoveMagicItemAttack()
+                  {
+                           Wizard wizard = new Wizard("gandalf");
+                           int AttackExpected = 80;
+                           Assert.AreEqual(AttackExpected, wizard.AttackPower);
 
-            int health = wizard.Health;
-            int defensePower = 0;
-            foreach (IItem item in wizard.Items)
-            {
-                if (item is IDefenseItem)
-                {
-                    defensePower += ((IDefenseItem)item).DefensePower;
-                }
-            }
+                           wizard.RemoveItem(wizard.Items[0]);
+                           int AttackExpected2 = 0;
+                           Assert.AreEqual(AttackExpected2, wizard.AttackPower);
+                  }
+                  [Test]
+                  public void RemoveMagicItemDefense()
+                  {
+                           Wizard wizard = new Wizard("gandalf");
+                           int DefenseExpected = 30;
+                           Assert.AreEqual(DefenseExpected, wizard.DefensePower);
 
-            wizard.ReceiveAttack(health + defensePower);
+                           wizard.RemoveItem(wizard.Items[0]);
+                           int DefenseExpected2 = 0;
+                           Assert.AreEqual(DefenseExpected2, wizard.DefensePower);
+                  }
+                  [Test]
+                  public void AttackWithHelth()
+                  {
+                           Wizard wizard = new Wizard("gandalf");
+                           int Health = wizard.Health;
+                           int Defense = wizard.DefensePower;
+                           wizard.ReceiveAttack(Health + Defense);
 
-            bool expectedDeathValue = true;
-            bool actualDeathValue = wizard.IsDead;
+                           int HealthExpected = 0;
+                           
+                           wizard.ReceiveAttack(20);
+                           Assert.AreEqual(HealthExpected,wizard.Health);
+                  }
+                  [Test]
+                  public void AttackWithArmor()
+                  {
+                           Wizard wizard = new Wizard("gandalf");
+                           int Health = wizard.Health;
+                           int Defense = wizard.DefensePower;
+                           wizard.ReceiveAttack(10);
 
-            Assert.AreEqual(expectedDeathValue, actualDeathValue);
-        }
-
-        [Test]
-        public void TestAttackPowerSum()
-        {
-            //Desafio 11
-            Wizard wizard = new Wizard("Wizard");
-            int baseAtackPower = wizard.AttackPower;
-            MagicStick magicStick = new MagicStick();
-            Gauntlet gauntlet = new Gauntlet();
-
-            wizard.AddItem(new MagicStick());
-            wizard.AddItem(new Gauntlet());
-
-            int atackPowerFromItems = magicStick.AttackPower + gauntlet.AttackPower;
-            int expectedAtackPower = baseAtackPower + atackPowerFromItems;
-            int actualAtackPower = wizard.AttackPower;
-
-            Assert.AreEqual(expectedAtackPower, actualAtackPower);
-        }
-
-        [Test]
-        public void TestDefensePowerSum()
-        {
-            //Desafio 12
-            Wizard wizard = new Wizard("Wizard");
-            int baseDefensePower = wizard.DefensePower;
-            MagicStick magicStick = new MagicStick();
-            Armor armor = new Armor();
-
-            wizard.AddItem(new MagicStick());
-            wizard.AddItem(new Armor());
-
-            int defensePowerFromItems = magicStick.DefensePower + armor.DefensePower;
-            int expectedDefensePower = baseDefensePower + defensePowerFromItems;
-            int actualDefensePower = wizard.DefensePower;
-
-            Assert.AreEqual(expectedDefensePower, actualDefensePower);
-        }
-    }
+                           int HealthExpected = 80;
+                           
+                           Assert.AreEqual(HealthExpected,wizard.Health);
+                  }
+         }
 }
